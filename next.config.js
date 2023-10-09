503bcd2b462607c055d05293378df48127a699b6
vickzuzo <vickzuzo@gmail.com>
2023-10-09 21:25:07 +0100

    update: removed domain based images

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
